bf3143a6c880291639fe20d03eec419c56b06be8
chanori <komariyui222@gmail.com>
2019-12-24 01:04:55 +0900

    change label position

--- tkinter_practice.py
+++ tkinter_practice.py
@@ -8,6 +8,6 @@ root.title(u"window title")
 root.geometry("400x300")
 
 static1 = tk.Label(text=u"test", foreground="#ff69b4", background="#e0ffff")
-static1.pack()
+static1.place(x=20,y=270)
 
 root.mainloop()
