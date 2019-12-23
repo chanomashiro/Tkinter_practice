d09e30a1125547b2131aa649c03f0cc9c73871d6
chanori <komariyui222@gmail.com>
2019-12-24 01:09:23 +0900

    set entry

--- tkinter_practice.py
+++ tkinter_practice.py
@@ -7,7 +7,10 @@ root.title(u"window title")
 
 root.geometry("400x300")
 
-static1 = tk.Label(text=u"test", foreground="#ff69b4", background="#e0ffff")
-static1.place(x=20,y=270)
+static1 = tk.Label(text=u"entry")
+static1.pack()
+
+entry1 = tk.Entry()
+entry1.pack()
 
 root.mainloop()
