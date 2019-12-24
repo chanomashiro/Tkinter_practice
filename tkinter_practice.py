dede50c2efb0bf9f2b708b037a1b9dc6a27b699f
chanori <komariyui222@gmail.com>
2019-12-25 02:21:01 +0900

    add listbox

--- tkinter_practice.py
+++ tkinter_practice.py
@@ -13,35 +13,22 @@ def delete_entry(event):
     entry1.delete(0, tk.END)
 
 
-def showMessege(text):
-    tkm.showinfo("info", text)
-
-
 static1 = tk.Label(text=u"entry")
 static1.pack()
+
 
 entry1 = tk.Entry(width=50)
 entry1.insert(tk.END, u"hoge")
 entry1.pack()
 
+
 button1 = tk.Button(text=u"hoge button", width=50, command=lambda: showMessege(entry1.get()))
-#button1.bind("<Button-1>", delete_entry)
 button1.pack()
 
 
-tkm.showinfo("hoge", "hogehoge")
+listbox1 = tk.Listbox()
+listbox1.pack()
 
-tkm.showwarning("warning", "warning hoge")
-
-tkm.showerror("error", "error hoge")
-
-tkm.askyesno("yesno", "yesno hoge")
-
-tkm.askretrycancel("retrycancel", "hogehoge")
-
-tkm.askquestion("question", "hogehoge")
-
-tkm.askokcancel("okcancel", "hogehoge")
 
 
 root.mainloop()
