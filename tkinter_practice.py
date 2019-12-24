46db081d3e59d5ee5736363dfcd4acabca16892a
chanori <komariyui222@gmail.com>
2019-12-25 01:57:20 +0900

    add dialog display

--- tkinter_practice.py
+++ tkinter_practice.py
@@ -1,5 +1,6 @@
 import sys
 import tkinter as tk
+import tkinter.messagebox as tkm
 
 root = tk.Tk()
 
@@ -12,6 +13,10 @@ def delete_entry(event):
     entry1.delete(0, tk.END)
 
 
+def showMessege(text):
+    tkm.showinfo("info", text)
+
+
 static1 = tk.Label(text=u"entry")
 static1.pack()
 
@@ -19,8 +24,8 @@ entry1 = tk.Entry(width=50)
 entry1.insert(tk.END, u"hoge")
 entry1.pack()
 
-button1 = tk.Button(text=u"hoge button", width=50)
-button1.bind("<Button-1>", delete_entry)
+button1 = tk.Button(text=u"hoge button", width=50, command=lambda: showMessege(entry1.get()))
+#button1.bind("<Button-1>", delete_entry)
 button1.pack()
 
 
