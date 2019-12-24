8676c3e4ddb1e67bbe5d9ab8fd8acb2a1ff3c69b
chanori <komariyui222@gmail.com>
2019-12-25 02:35:50 +0900

    add delete button

--- tkinter_practice.py
+++ tkinter_practice.py
@@ -13,6 +13,11 @@ def addList(text):
     listbox1.insert(tk.END, text)
 
 
+def deleteSelectedList():
+    selectedIndex = tk.ACTIVE
+    listbox1.delete(selectedIndex)
+
+
 static1 = tk.Label(text=u"entry")
 static1.pack()
 
@@ -22,8 +27,12 @@ entry1.insert(tk.END, u"hoge")
 entry1.pack()
 
 
-button1 = tk.Button(text=u"hoge button", width=50, command=lambda: addList(entry1.get()))
+button1 = tk.Button(text=u"add", width=50, command=lambda: addList(entry1.get()))
 button1.pack()
+
+
+button2 = tk.Button(text=u"delete", width=50, command=lambda: deleteSelectedList())
+button2.pack()
 
 
 listbox1 = tk.Listbox(width=50, height=10)
